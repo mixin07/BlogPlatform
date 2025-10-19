dd0d3651e4a26fce837db92859ad144dab3a8b3d
mixin07 <jeyavelan68@gmail.com>
2025-10-19 18:10:14 +0530

    commit for test

--- client/app/member/dashboard/page.tsx
+++ client/app/member/dashboard/page.tsx
@@ -3,6 +3,7 @@ export default function MemberDashboard() {
     <div>
       <h1>Member Dashboard</h1>
       <p>Here members will create / edit / submit blogs. (Frontend team: implement UI & connect to /api/blogs)</p>
+      <p>Note: This page is only accessible to authenticated members.</p>
     </div>
   );
 }
